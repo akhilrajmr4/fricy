83ac0dfc50c9c322979ca024142b557352744d68
akhilrajmr4 <akhi.akhilraj18@gmail.com>
2021-11-16 13:08:19 +0530

    contactus changed

--- contactus.php
+++ contactus.php
@@ -3,13 +3,14 @@
     error_reporting(E_ALL);
 	if(isset($_POST['submit']))
 	{
-		$to="akhi.akhilraj18@gmail.com"; 
+		$to="fricy.in@gmail.com"; 
 
 		$Name = $_POST["name"];
 		$email = $_POST["email"];
 		$subject = $_POST["subject"];
 		$message1 = $_POST["message"];
-		$message ="Name : ".$Name."<br>"."Email : "$email."<br>"."Subject : ".$subject."<br>"."Message: ".$message1;
+		
+		$message = "Name : ".$Name."\nEmail : ".$email."\nSubject : ".$subject."\nMessage: ".$message1;
 		// a random hash will be necessary to send mixed content
 		$separator = md5(time());
 
@@ -17,19 +18,13 @@
 		$eol = "\r\n";
 
 		// main header (multipart mandatory)
-		$headers = "From: name <fricy@test.com>" . $eol;
+		$headers = "From: name <fricy.in>" . $eol;
 		$headers .= "MIME-Version: 1.0" . $eol;
 		$headers .= "Content-Type: multipart/mixed; boundary=\"" . $separator . "\"" . $eol;
 		$headers .= "Content-Transfer-Encoding: 7bit" . $eol;
-		$headers .= "This is a MIME encoded message." . $eol;
+		$headers .= "This message from fricy website." . $eol;
 
-		// message
-		$body = "--" . $separator . $eol;
-		$body .= "Content-Type: text/plain; charset=\"iso-8859-1\"" . $eol;
-		$body .= "Content-Transfer-Encoding: 8bit" . $eol;
-		$body .= $message . $eol;
-
-
+		
 		if(mail($to, $subject, $message, $headers))
 		{
 				echo "<script>
@@ -44,6 +39,8 @@
 				window.location.href = 'index.html';
 				</script>";
 			}
+
+
 	}
 	else
 	{
@@ -52,4 +49,3 @@
 		window.location.href = 'index.html';
 		</script>";
 	}
-?>
